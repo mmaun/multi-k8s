6c09807da7afdb5a020c374aa00c9b5b4e52164f
Mani Maun <m_maun@hotmail.com>
2019-02-16 12:00:27 +0000

    added deployment steps

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t maunconsultancy/multi-client:latest -t maunconsultancy/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t maunconsultancy/multi-server:latest -t maunconsultancy/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t maunconsultancy/multi-worker:latest -t maunconsultancy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push maunconsultancy/multi-client:latest
+docker push maunconsultancy/multi-server:latest
+docker push maunconsultancy/multi-worker:latest 
+
+docker push maunconsultancy/multi-client:$SHA
+docker push maunconsultancy/multi-server:$SHA
+docker push maunconsultancy/multi-worker:$SHA 
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=maunconsultancy/multi-server:$SHA
+kubectl set image deployment/client-deployment client=maunconsultancy/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=maunconsultancy/multi-worker:$SHA
